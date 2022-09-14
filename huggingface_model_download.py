2b39de182f2ddefe221df84f52cc554d8f5c9bc3
yslee <yslee.dev@gmail.com>
2022-09-15 00:48:52 +0900

    feat: stable diffusion model download

---
+++ huggingface_model_download.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    # download stable diffusion model to local huggingface cache dir
+    from core.dependencies import models
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import uvicorn
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
-    app = 'app.server:app'
+    app = "app.server:app"
     uvicorn.run(
         app,
         port=8000,
-        host='0.0.0.0',
+        host="0.0.0.0",
         workers=1,
     )
